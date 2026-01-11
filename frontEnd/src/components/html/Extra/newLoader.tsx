a6fe9f10843db789a50ba08810f1f099019aca01
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2026-01-12 03:38:47 +0530

    Changes

--- frontEnd/src/components/html/Extra/newLoader.tsx
+++ frontEnd/src/components/html/Extra/newLoader.tsx
@@ -1,13 +1,18 @@
-import { dottedBg } from '../../../Styles'
-import '../../css/Extra/newLoader.css'
+import { dottedBg } from "../../../Styles"
+import logo from "/src/assets/NEAR_LOGO-nobg.svg" ;
 
 const newLoader = () => {
   return (
-    <div style={dottedBg} className={`w-full h-screen flex-mid`}>
-      <div id='newLoadingDiv' className={`relative h-px w-[320px] overflow-visible`}>
-        <div className='w-[180px] h-20 border bg-(--green) absolute left-0 top-1/2 -translate-y-1/2 skew-x-[-30deg]' id='leftLoadingDiv'></div>
-        <div className='w-[180px] h-20 border bg-(--green) absolute right-0 top-1/2 -translate-y-1/2 skew-x-[30deg]' id='rightLoadingDiv'></div>
-      </div>  
+    <div className="w-screen h-screen flex-mid" style={dottedBg}>
+      <div className="relative h-px w-36">
+        <div className="w-12 h-8 flex-mid absolute left-0 top-1/2 -translate-y-1/2">
+          <img src={logo} alt="NEAR logo"/>
+          </div>
+          <div className="h-full w-16 bg-white"></div>
+        <div className="w-12 h-8 flex-mid absolute right-0 top-1/2 -translate-y-1/2">
+          <img src={logo} alt="NEAR logo" />
+          </div>
+      </div>
     </div>
   )
 }
