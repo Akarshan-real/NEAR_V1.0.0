730e1be122b77f074dbed78498d19bc752466342
Akarshan Chakraborty <akarshanchakraborty@gmail.com>
2026-01-16 20:57:21 +0530

    Update main.tsx

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -2,13 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/Router.tsx'
 import { AuthProvider } from './contexts/Auth/AuthContext.tsx'
-import { LoaderProvider } from './contexts/loader/LoaderContext.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
     <AuthProvider>
-        <LoaderProvider>
+        
             <RouterProvider router={router} />
-        </LoaderProvider>
+       
     </AuthProvider>
 )
